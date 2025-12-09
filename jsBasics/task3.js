0022d4169fc3e2d1b9ed85f6c2f037efb147bb5a
DSpyrydonov <dakurlzz1543@gmail.com>
2025-12-09 16:35:22 +0200

    Solution for task 3, start task 4

--- jsBasics/task3.js
+++ jsBasics/task3.js
@@ -1,1 +1,10 @@
-Hello
+let age = 18
+let persone1 = 17
+let persone2 = 18
+let persone3 = 19
+let randomAge = Math.round(Math.random() * 100)
+
+console.log(persone1 >= age ? "Person are adult" : "Person aren't adult")
+console.log(persone2 >= age ? "Person are adult" : "Person aren't adult")
+console.log(persone3 >= age ? "Person are adult" : "Person aren't adult")
+console.log(randomAge >= age ? `Age is  ${randomAge}, Person are adult` : `Age is  ${randomAge}, Person aren't adult`)
