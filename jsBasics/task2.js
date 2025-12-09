440bce9394d4e46427e6da81df8a302495a736e2
DSpyrydonov <dakurlzz1543@gmail.com>
2025-12-09 16:08:10 +0200

    Solution for task 2, start task 3

--- jsBasics/task2.js
+++ jsBasics/task2.js
@@ -1,1 +1,7 @@
-Hello
+let persone1 = 'Jhon'
+let persone2 = 'Matt'
+
+let greatings = 'Hello ' + persone1 + " and " + persone2 + ", glad to see you!"
+
+console.log(greatings)
+console.log(`Hello ${persone1} and ${persone2}, glad to see you!`)
