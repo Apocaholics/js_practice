d95b5716cf272301ae1d83c3bb3100238ba9de02
DSpyrydonov <dakurlzz1543@gmail.com>
2025-12-25 17:13:14 +0200

    Solution for task1

--- jsAdvancedFunctionUsage/task1.js
+++ jsAdvancedFunctionUsage/task1.js
@@ -1,1 +1,22 @@
-Hello
+function handleNum(number, handleEven, handleOdd) {
+    try {
+        if (number % 2 === 0) {
+            handleEven(number)
+        } else {
+            handleOdd(number)
+        }
+    } catch (e) {
+
+    }
+}
+
+function handleEven(number) {
+    console.log(`${number} is an even number`)
+}
+
+function handleOdd(number) {
+    console.log(`${number} is an odd number`)
+}
+
+let number = Math.round(Math.random() * 100)
+handleNum(number, handleEven, handleOdd)
