67cd35cb584326595b481d2f418563be58791bab
DSpyrydonov <dakurlzz1543@gmail.com>
2025-12-27 12:33:15 +0200

    Solution for task1

--- jsArrays/task1.js
+++ jsArrays/task1.js
@@ -1,1 +1,19 @@
-Hello
+const numbers = [2, -5, 0, 7, -3, 0, 10, -8]
+
+let positiveCount = 0
+let negativeCount = 0
+let zeroCount = 0
+
+for (const number of numbers) {
+    if (number > 0) {
+        positiveCount++
+    } else if (number === 0) {
+        zeroCount++
+    } else {
+        negativeCount++
+    }
+}
+
+console.log(`Кількість позитивних чисел: ${positiveCount}`)
+console.log(`Кількість негативних чисел: ${negativeCount}`)
+console.log(`Кількість нульових чисел: ${zeroCount}`)
