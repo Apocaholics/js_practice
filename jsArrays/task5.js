e3ab8e684a328dd2ff65c691d2c731fb865d3242
DSpyrydonov <dakurlzz1543@gmail.com>
2025-12-28 10:34:27 +0200

    Solution for task5

---
+++ jsArrays/task5.js
@@ -0,0 +1,6 @@
+const firstArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const secondArray = [20, 30, 40, 50]
+
+const concatenatedArray = firstArray.concat(secondArray)
+
+console.log(concatenatedArray)
