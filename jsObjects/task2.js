e03be5dceeba4150df2e04c71cc2cd248d909d3d
D.Spyrydonov <dakurlzz1543@gmail.com>
2026-01-16 15:51:52 +0200

    Solution for task 2

---
+++ jsObjects/task2.js
@@ -0,0 +1,9 @@
+const book = {
+    title: "Head First JavaScript Programming: A Brain-Friendly Guide",
+    author: "Eric Freeman",
+    year: 2014
+}
+
+let { title, author } = book
+
+console.log(`Book: ${title} \nAuthor: ${author}`)
