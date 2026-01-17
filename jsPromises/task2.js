f953eb301c94211c7af1293e931836e874f9cb0f
D.Spyrydonov <dakurlzz1543@gmail.com>
2026-01-17 18:15:07 +0200

    Solution for task 3, fix task 2

--- jsPromises/task2.js
+++ jsPromises/task2.js
@@ -1,16 +1,29 @@
 function getToDo() {
     return fetch("https://jsonplaceholder.typicode.com/todos/1")
+        .then(response => response.json())
 }
 
 function getUser() {
     return fetch("https://jsonplaceholder.typicode.com/users/1")
+        .then(response => response.json())
 }
 
 const promiseCollection1 = Promise.all([getToDo(), getUser()])
     .then(([toDo, user]) => {
-        console.log(toDo)
-        console.log(user)
+        const toDoResponse = toDo
+        const userResponse = user
+
+        return { toDoResponse, userResponse }
+    })
+    .then(({ toDoResponse, userResponse }) => {
+        console.log(toDoResponse)
+        console.log(userResponse)
     })
 
 const promiseCollection2 = Promise.race([getToDo(), getUser()])
-    .then(response => console.log(response))
+    .then((response) => {
+        const data = response
+
+        return data
+    })
+    .then((data) => console.log(data))
